644615a0e67d5b5878901a1eaefe226b0bca6adb
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-05-15 16:47:45 -0500

    Se agregó genericObject, ahora presenta métodos diferentes a los que ya tiene por defecto, son básicos

--- GenericObject.java
+++ GenericObject.java
@@ -1,7 +1,22 @@
-import java.util.*;
+public class GenericObject<T> {
 
-public class GenericObject<E> {
+    private T first;
+    private T existe;
+
     public GenericObject() {
+        first = null;
+        existe = (T) "Si, existe!";
+    }
 
+    public void setFirst(T value) {
+        first = value;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T itExist() {
+        return existe;
     }
 }
