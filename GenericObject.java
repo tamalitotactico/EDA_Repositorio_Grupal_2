0cb621fa025b142e0d4e6f0192d476295a7d6b4b
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-05-15 15:57:35 -0500

    Se agregó el objeto genérico, junto con todos los métodos que utilizará, los metodos no tienen declarado las devoluciones ni las entradas aún.

---
+++ GenericObject.java
@@ -0,0 +1,78 @@
+import java.util.List;
+
+public class GenericObject<T> {
+    public GenericObject() {
+    }
+
+    public void size() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public void contains() {
+
+    }
+
+    public void add() {
+
+    }
+
+    public void add(int a) {
+
+    }
+
+    public void addAll() {
+
+    }
+
+    public void addAll(int b) {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public void remove(int a) {
+
+    }
+
+    public void removeAll() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void equals() {
+
+    }
+
+    public void get() {
+
+    }
+
+    public void set() {
+
+    }
+
+    public void containsAll() {
+
+    }
+
+    public void indexOf() {
+
+    }
+
+    public void lastIndexOf() {
+
+    }
+
+    public void subList() {
+
+    }
+}
