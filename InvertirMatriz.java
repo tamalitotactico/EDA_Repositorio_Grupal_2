6f7352b26bab10f6aa1f52d8ffbd85c84d6ac507
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2022-05-15 18:25:08 -0500

    Implementé le método invertirArray

--- InvertirMatriz.java
+++ InvertirMatriz.java
@@ -4,4 +4,11 @@ public class InvertirMatriz {
         
     }
 
+    static int[] invertirArray(int[] A) {
+        int[] Ain = new int[A.length];
+        for(int i=0, j=A.length-1; i<A.length; i++, j--) 
+            Ain[i] = A[j];
+        return Ain;
+    }
+
 }
