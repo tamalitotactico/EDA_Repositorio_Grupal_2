b5a02eb1c26f0254d6d5cef92ae6621854504364
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2022-05-15 18:30:20 -0500

    Añadí un interfaz para el funcionamiento del método

--- InvertirMatriz.java
+++ InvertirMatriz.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class InvertirMatriz {
 
     public static void main(String[]args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño de la matriz: ");
+        int n = s.nextInt();
+        int[] array = new int[n];
+        for(int i=0; i<n; i++) {
+	        System.out.print("Ingrese el " + (i+1) + "º valor : ");
+	        array[i] = s.nextInt();
+        }
+        array = invertirArray(array);
+        System.out.println("Matriz invertida:");
+        for(int i=0; i<array.length; i++)
+	        System.out.print(array[i] + "\t");
     }
 
     static int[] invertirArray(int[] A) {
